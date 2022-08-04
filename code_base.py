12ccbc9e29575a324e92f24fe4f3acce563e8ace
LeoPSavo <leopsvo@gmail.com>
2022-08-04 14:08:40 -0300

    first commit

---
+++ code_base.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.express as px
+#import streamlit as st
+
+
+#LENDO O DATASET
+df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
+
+#MELHORANDO O NOME DAS COLUNAS DA TABELA
+df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
+
+#SELECÃO DO ESTADO
+state  = 'MS'
+estados = list(df['state'].unique())
+
+#SELEÇÃO DA COLUNA
+column ='Casos por 100 mil habitantes'
+colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
+
+#SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO 
+df = df[df['state'] == state]
+
+
+fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
+fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
+
+print('DADOS COVID - BRASIL')
+print('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem.')
+
+fig.show()
+
+print('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
